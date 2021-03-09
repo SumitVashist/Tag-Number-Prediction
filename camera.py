665ee765333e4031cd6827034ccfc2c7ed761c73
Sumit Kumar <skvd07@gmail.com>
2021-03-09 12:42:30 +0530

    Create camera.py
    Triggering camera using push button on jetson nano

---
+++ camera.py
@@ -0,0 +1,42 @@
+import cv2
+import RPi.GPIO as GPIO
+import traceback
+
+dispH=1280
+dispW=960
+flip=2
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+but_pin = 15  # Board pin 15
+GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
+GPIO.setup(but_pin, GPIO.IN)  # button pin set as input
+print("Starting demo now! Press CTRL+C to exit")
+
+i = 1
+
+try:
+	while i<2:
+		value = GPIO.input(but_pin)
+		#print(type(value))
+		print("Value read from pin {} : {}".format(but_pin, value))
+		if value ==1:
+			#print("assfcdgsdg")
+			
+			camera = cv2.VideoCapture(camSet)
+			return_value, image = camera.read()
+			cv2.imwrite(str(i+1)+'.png', image)
+			cv2.imshow("Image",image)
+			i+=1
+			camera.release()
+			cv2.destroyAllWindows()
+	#		break		
+except:
+    print(traceback.format_exc())
+finally:
+	GPIO.cleanup()
+	camera.release()
+	cv2.destroyAllWindows()
+
+	
+	
+
+
